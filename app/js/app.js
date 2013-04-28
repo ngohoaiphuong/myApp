dc3fc2b6a7c338e1ef856eb5aa4977502cd91fb0
Ngo Hoai Phuong <ngohoai.phuong@gmail.com>
2013-04-28 23:19:06 +0700

    remove app/js

--- app/js/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* App Module */
-
-angular.module('myApp', []).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-        otherwise({redirectTo: '/'});
-}]);
